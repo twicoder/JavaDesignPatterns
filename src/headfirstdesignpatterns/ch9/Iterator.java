a3cb43cdc6d956eb69be9254c3a8209672b8c916
Chinwe <renqingwei123@163.com>
2018-01-21 23:40:43 +0800

    iterator pattern v1

---
+++ src/headfirstdesignpatterns/ch9/Iterator.java
@@ -0,0 +1,6 @@
+package headfirstdesignpatterns.ch9;
+
+public interface Iterator {
+    boolean hasNext();
+    Object next();
+}
---
+++ src/headfirstdesignpatterns/ch9/PancakeHouseMenuIterator.java
@@ -0,0 +1,29 @@
+package headfirstdesignpatterns.ch9;
+
+import java.awt.*;
+import java.util.ArrayList;
+
+public class PancakeHouseMenuIterator implements Iterator {
+    ArrayList menuItems;
+    int position = 0;
+
+    public PancakeHouseMenuIterator(ArrayList menuItems) {
+        this.menuItems = menuItems;
+    }
+
+    @Override
+    public boolean hasNext() {
+        if(position >= menuItems.size() || menuItems.get(position) == null){
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    @Override
+    public Object next() {
+        MenuItem menuItem = (MenuItem)menuItems.get(position);
+        position = position + 1;
+        return menuItem;
+    }
+}
